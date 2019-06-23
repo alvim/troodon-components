23e96e146c6660e368fafc493e5fdbd9ea3c102e
Mauricio Alvim <mauricio.alvim@maxmilhas.com.br>
2019-06-23 10:38:04 -0300

    Import React.

--- dist/index.js
+++ dist/index.js
@@ -4,20 +4,25 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 var stock = {
   Paragraph: function Paragraph(_ref) {
     var children = _ref.children;
-    return React.createElement("p", null, children);
+    return _react["default"].createElement("p", null, children);
   },
   Divider: function Divider() {
-    return React.createElement("hr", null);
+    return _react["default"].createElement("hr", null);
   },
   Span: function Span() {
-    return React.createElement("span", null);
+    return _react["default"].createElement("span", null);
   },
   Banner: function Banner(_ref2) {
     var children = _ref2.children;
-    return React.createElement("div", null, children);
+    return _react["default"].createElement("div", null, children);
   }
 };
 var _default = stock;
